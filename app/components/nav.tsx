a22874e0095ba6acd3bfe317fe1c53a21bc4de60
Jon Wilson <jonwilsonmn@gmail.com>
2024-06-22 19:15:03 -0500

    WIP adding a glossary page

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,25 +1,28 @@
-'use client'
+'use client';
 
-import { useEffect, useState } from 'react'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Nav() {
-  const pathname = usePathname()
-  const [activeItem, setActiveItem] = useState('')
+  const pathname = usePathname();
+  const [activeItem, setActiveItem] = useState('');
 
   useEffect(() => {
     switch (pathname) {
       case '/':
-        setActiveItem('Solid Documentation')
-        break
+        setActiveItem('Solid Documentation');
+        break;
       case '/add-resource':
-        setActiveItem('Add a Resource')
-        break
+        setActiveItem('Add a Resource');
+        break;
+      case '/glossary':
+        setActiveItem('Glossary');
+        break;
       default:
-        console.log(`404`)
+        console.log(`404`);
     }
-  }, [pathname])
+  }, [pathname]);
 
   return (
     <div className="flex bg-primary-800 mb-10 pl-5 py-3">
@@ -43,6 +46,16 @@ export default function Nav() {
       >
         Add a Resource
       </Link>
+      <Link
+        className={
+          activeItem === 'Glossary'
+            ? 'text-2xl mr-4 text-primary-300 text-shadow-neonGlow'
+            : 'text-2xl mr-4 text-primary-300 hover:text-shadow-neonGlow'
+        }
+        href="/glossary"
+      >
+        Glossary
+      </Link>
     </div>
-  )
+  );
 }
---
+++ app/glossary/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="mt-10 ml-10">
+      <p>Glossary</p>
+    </div>
+  );
+}
