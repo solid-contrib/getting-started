e2787b206e9851df46b9dc6a5e605e1789bf5c39
Jon Wilson <jonwilsonmn@gmail.com>
2024-08-18 13:36:50 -0500

    WIP updates to glossary page

--- app/glossary/page.tsx
+++ app/glossary/page.tsx
@@ -3,10 +3,28 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
+// create entries in pod, using vocabulary
+// get them on page load or at build time
+// console log to see structure
+
 export default function Page() {
+  const glossaryList = [{ name: 'firstentry', url: '' }];
   return (
-    <div className="mt-10 ml-10">
-      <p>Glossary</p>
+    <div className="mt-10 ml-10 max-w-max">
+      <div className="bg-primary-800 flex text-3xl text-primary-50 p-3">
+        Glossary
+      </div>
+      {glossaryList.length &&
+        glossaryList.map((entry) => (
+          <div className="pl-3 font-bold mt-2" key={entry.name}>
+            <Link
+              className="bg-accent1-400 p-1 hover:text-white hover:bg-accent1-700"
+              href={entry.url || ''}
+            >
+              {entry.name}
+            </Link>
+          </div>
+        ))}
     </div>
   );
 }
